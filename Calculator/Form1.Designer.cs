f0fb3836bce206c5585345b620b3cefed1ceeb9f
Kari Piukka <kari.piukka@gmail.com>
2019-02-28 11:14:53 +0200

    Create git

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Calculator {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_decimal = new System.Windows.Forms.Button();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_mul = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.txt_screen = new System.Windows.Forms.TextBox();
+            this.txt_stored = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_0
+            // 
+            this.btn_0.Location = new System.Drawing.Point(13, 389);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(50, 50);
+            this.btn_0.TabIndex = 0;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = true;
+            this.btn_0.Click += new System.EventHandler(this.btn_0_Click);
+            // 
+            // btn_decimal
+            // 
+            this.btn_decimal.Location = new System.Drawing.Point(69, 389);
+            this.btn_decimal.Name = "btn_decimal";
+            this.btn_decimal.Size = new System.Drawing.Size(50, 50);
+            this.btn_decimal.TabIndex = 1;
+            this.btn_decimal.Text = ".";
+            this.btn_decimal.UseVisualStyleBackColor = true;
+            this.btn_decimal.Click += new System.EventHandler(this.btn_decimal_Click);
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(125, 389);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(50, 50);
+            this.btn_calc.TabIndex = 2;
+            this.btn_calc.Text = "=";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(181, 389);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(50, 50);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "+";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(13, 333);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(50, 50);
+            this.btn_1.TabIndex = 4;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(69, 333);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(50, 50);
+            this.btn_2.TabIndex = 5;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(125, 333);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(50, 50);
+            this.btn_3.TabIndex = 6;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(181, 333);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(50, 50);
+            this.btn_sub.TabIndex = 7;
+            this.btn_sub.Text = "-";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(13, 277);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(50, 50);
+            this.btn_4.TabIndex = 8;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(69, 277);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(50, 50);
+            this.btn_5.TabIndex = 9;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(125, 277);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(50, 50);
+            this.btn_6.TabIndex = 10;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // btn_mul
+            // 
+            this.btn_mul.Location = new System.Drawing.Point(181, 277);
+            this.btn_mul.Name = "btn_mul";
+            this.btn_mul.Size = new System.Drawing.Size(50, 50);
+            this.btn_mul.TabIndex = 11;
+            this.btn_mul.Text = "x";
+            this.btn_mul.UseVisualStyleBackColor = true;
+            this.btn_mul.Click += new System.EventHandler(this.btn_mul_Click);
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(13, 221);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(50, 50);
+            this.btn_7.TabIndex = 12;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(69, 221);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(50, 50);
+            this.btn_8.TabIndex = 13;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(125, 221);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(50, 50);
+            this.btn_9.TabIndex = 14;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Location = new System.Drawing.Point(181, 221);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(50, 50);
+            this.btn_div.TabIndex = 15;
+            this.btn_div.Text = "/";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // txt_screen
+            // 
+            this.txt_screen.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_screen.Location = new System.Drawing.Point(13, 174);
+            this.txt_screen.Name = "txt_screen";
+            this.txt_screen.ReadOnly = true;
+            this.txt_screen.Size = new System.Drawing.Size(218, 26);
+            this.txt_screen.TabIndex = 16;
+            this.txt_screen.Text = "0";
+            this.txt_screen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_stored
+            // 
+            this.txt_stored.Location = new System.Drawing.Point(13, 13);
+            this.txt_stored.Multiline = true;
+            this.txt_stored.Name = "txt_stored";
+            this.txt_stored.Size = new System.Drawing.Size(218, 155);
+            this.txt_stored.TabIndex = 17;
+            this.txt_stored.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 450);
+            this.Controls.Add(this.txt_stored);
+            this.Controls.Add(this.txt_screen);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_mul);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.btn_decimal);
+            this.Controls.Add(this.btn_0);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_decimal;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_mul;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.TextBox txt_screen;
+        private System.Windows.Forms.TextBox txt_stored;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator {
+    public partial class Form1 : Form {
+
+        List<string> stored;
+        private bool clearField;
+
+        public Form1() {
+            InitializeComponent();
+            this.KeyDown += Form1_Keydown;
+            stored = new List<string>();
+        }
+
+        /* KEYBOARD SHORTCUTS */
+        private void Form1_Keydown(object sender, KeyEventArgs e) {
+            switch (e.KeyValue) {
+                case 48:
+                case 96:
+                    addChar('0');
+                    break;
+                case 49:
+                case 97:
+                    addChar('1');
+                    break;
+                case 50:
+                case 98:
+                    addChar('2');
+                    break;
+                case 51:
+                case 99:
+                    addChar('3');
+                    break;
+                case 52:
+                case 100:
+                    addChar('4');
+                    break;
+                case 53:
+                case 101:
+                    addChar('5');
+                    break;
+                case 54:
+                case 102:
+                    addChar('6');
+                    break;
+                case 55:
+                case 103:
+                    addChar('7');
+                    break;
+                case 56:
+                case 104:
+                    addChar('8');
+                    break;
+                case 57:
+                case 105:
+                    addChar('9');
+                    break;
+                case 110:
+                case 188:
+                case 190:
+                    addChar('.');
+                    break;
+                case 107:
+                case 187:
+                    addChar('+');
+                    break;
+                case 109:
+                case 189:
+                    addChar('-');
+                    break;
+                case 106:
+                    addChar('x');
+                    break;
+                case 111:
+                    addChar('/');
+                    break;
+                case 13:
+                    Calculate();
+                    break;
+                default:
+                    Console.WriteLine(e.KeyValue);
+                    break;
+            }
+        }
+
+        /* CHECK IF CHAR IS ALLOWED */
+        private bool charIsAllowed(char character) {
+            return character != '.' &&
+                character != '/' &&
+                character != 'x' &&
+                character != '-' &&
+                character != '+';
+        }
+
+        /* CHECK IF CHARACTER IS AN OPERATOR */
+        private bool isOperator(char character) {
+            return character == '/' ||
+                character == 'x' ||
+                character == '+' ||
+                character == '-';
+        }
+
+        /* CHECK IF STRING CONTAINS AN OPERATOR */
+        private bool hasOperator(string s) {
+            return s.Contains('+') ||
+                s.Contains('-') ||
+                s.Contains('x') ||
+                s.Contains('/');
+        }
+
+        /* CHECK IF STRING ALREADY HAS A CHARACTER THAT IS ONLY ALLOWED ONCE */
+        private bool hasCharAllowedOnce() {
+            return txt_screen.Text.Contains(".") ||
+            txt_screen.Text.Contains("+") ||
+            txt_screen.Text.Contains("-") ||
+            txt_screen.Text.Contains("/") ||
+            txt_screen.Text.Contains("x");
+        }
+
+        /* CALCULATE */
+        private void Calculate() {
+            string[] numbers;
+
+            stored.Add(txt_screen.Text);
+            txt_stored.Lines = stored.ToArray<string>();
+
+            int lastIndex = stored.Count - 1;
+            string operation = stored[lastIndex - 1] + stored[lastIndex];
+
+            Console.WriteLine(operation);
+
+            if (operation.Contains("+")) {
+                numbers = operation.Split('+');
+                txt_screen.Text = (Convert.ToDouble(numbers[0]) + Convert.ToDouble(numbers[1])).ToString();
+            } else if (operation.Contains("-")) {
+                numbers = operation.Split('-');
+                txt_screen.Text = (Convert.ToDouble(numbers[0]) - Convert.ToDouble(numbers[1])).ToString();
+            } else if (operation.Contains("x")) {
+                numbers = operation.Split('x');
+                txt_screen.Text = (Convert.ToDouble(numbers[0]) * Convert.ToDouble(numbers[1])).ToString();
+            } else if (operation.Contains("/")) {
+                numbers = operation.Split('/');
+                txt_screen.Text = (Convert.ToDouble(numbers[0]) / Convert.ToDouble(numbers[1])).ToString();
+            }
+
+            // Store calculation to list and reprint the stored list
+            stored.Add("= " + txt_screen.Text);
+            txt_stored.Lines = stored.ToArray<string>();
+        }
+
+        /* METHOD FOR ADDING CHARACTERS */
+        private void addChar(char character) {
+
+            /* If character to be added is an operator and current text doesn't contain
+             * an operator, add said operator 
+             * This is to avoid multiple operators on single line, like +++-/5+--+ */
+            if (isOperator(character) && !hasOperator(txt_screen.Text)) {
+                stored.Add(txt_screen.Text);
+                txt_stored.Lines = stored.ToArray();
+                txt_screen.Text = character.ToString();
+            }
+
+            /* If character to be added is a comma and the current text doesn't contain
+             * a comma, add comma
+             * This is to avoid values with multiple commas, ie. "1....2.3.5"*/
+            if (character == '.' && !txt_screen.Text.Contains('.')) {
+                txt_screen.Text += character.ToString();
+            }
+
+            /* If character to be added isn't an operator or a comma
+             AND
+             1) text equals "0", then replace with character
+             2) text doesn't equal "0", then append character
+             This is to avoid strings like "00000000" and "0123"
+             */
+            if (!isOperator(character) && character != '.') {
+                if (txt_screen.Text.Equals("0")) {
+                    txt_screen.Text = character.ToString(); // if char = '0', this might be wasteful
+                } else {
+                    txt_screen.Text += character;
+                }
+            }
+
+            //txt_screen.Text += character.ToString();
+            // If textbox contains only 0:
+            // - add 0: Do nothing
+            // - add '.': Add period
+            // - add number 1-9: Replace with given number
+            // If textbox already has '.', do not add '.'
+
+            /*if (txt_screen.Text.Equals("0")) {
+                switch (character) {
+                    case '0':
+                        break;
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        txt_screen.Text = character.ToString();
+                        break;
+                    case '.':
+                        txt_screen.Text += ".";
+                        break;
+                }
+            } else if (hasCharAllowedOnce()) {
+                if (isOperator(character)) {
+                    stored.Add(txt_screen.Text);
+                    txt_stored.Lines = stored.ToArray<string>();
+                    Console.WriteLine(stored.ToString());
+                    txt_screen.Text += Environment.NewLine + character + " ";
+                } else if (character != '.') {
+                    txt_screen.Text += character;
+                }
+            } else {
+                if (isOperator(character)) {
+                    stored.Add(txt_screen.Text);
+                    txt_stored.Lines = stored.ToArray<string>();
+                    Console.WriteLine(stored.ToString());
+                    txt_screen.Text = character + " ";
+                } else if (character != '.') {
+                    txt_screen.Text += character;
+                }
+            }*/
+        }
+
+        /* BUTTON FUNCTIONS */
+        private void btn_1_Click(object sender, EventArgs e) {
+            addChar('1');
+        }
+
+        private void btn_2_Click(object sender, EventArgs e) {
+            addChar('2');
+        }
+
+        private void btn_3_Click(object sender, EventArgs e) {
+            addChar('3');
+        }
+
+        private void btn_4_Click(object sender, EventArgs e) {
+            addChar('4');
+        }
+
+        private void btn_5_Click(object sender, EventArgs e) {
+            addChar('5');
+        }
+
+        private void btn_6_Click(object sender, EventArgs e) {
+            addChar('6');
+        }
+
+        private void btn_7_Click(object sender, EventArgs e) {
+            addChar('7');
+        }
+
+        private void btn_8_Click(object sender, EventArgs e) {
+            addChar('8');
+        }
+
+        private void btn_9_Click(object sender, EventArgs e) {
+            addChar('9');
+        }
+
+        private void btn_0_Click(object sender, EventArgs e) {
+            addChar('0');
+        }
+
+        private void btn_decimal_Click(object sender, EventArgs e) {
+            addChar('.');
+        }
+
+        private void btn_div_Click(object sender, EventArgs e) {
+            addChar('/');
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e) {
+            addChar('x');
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e) {
+            addChar('-');
+        }
+
+        private void btn_add_Click(object sender, EventArgs e) {
+            addChar('+');
+        }
+
+        private void btn_calc_Click(object sender, EventArgs e) {
+            Calculate();
+        }
+    }
+}
